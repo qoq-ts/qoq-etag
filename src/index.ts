71bcdf6480c795ffe49a327861ccc86bebfd49ff
原罪 <531362022@qq.com>
2021-04-01 22:54:43 +0800

    refactor: Make @types/koa-etag as dependency

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import { Slot } from 'qoq';
 import conditional from 'koa-conditional-get';
 import etag from 'koa-etag';
-
-export interface EtagOptions {
-  weak?: boolean;
-}
+import { Options } from 'etag';
 
 export class Etag extends Slot<Slot.Web> {
-  constructor(options: EtagOptions = {}) {
+  constructor(options: Options = {}) {
     super();
 
     this
